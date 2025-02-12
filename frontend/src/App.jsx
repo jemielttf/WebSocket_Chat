19bae4426658a6e19ccd2a442c6294a1daef56e9
jemielttf <4700417+jemielttf@users.noreply.github.com>
2025-02-12 23:45:20 +0900

    update: Enterキーでチャット送信に対応
    日本語の入力変換時の誤送信、Safariの例外動作に対応

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 export default function App() {
 	const [messages, setMessages] = useState([]);
 	const [input, setInput] = useState("");
 	const [ws, setWs] = useState(null);
+
+	const msgInputRef = useRef(null); // refを追加
+	const isComposing = useRef(false); // useRefで状態管理
 
 	useEffect(() => {
 		const socket = new WebSocket("ws://localhost:7654");
@@ -29,7 +32,33 @@ export default function App() {
 
 		setWs(socket);
 
-		return () => socket.close();
+		// マウント時に#MsgInputを代入
+		const msgInput = msgInputRef.current;
+		let timeout_id = 0;
+
+		const handleCompositionStart = () => {
+			clearTimeout(timeout_id);
+			console.log(`Now set to isComposing.current = true`);
+			isComposing.current = true;
+		};
+
+		const handleCompositionEnd = () => {
+			timeout_id = setTimeout(function() {
+				console.log(`Now set to isComposing.current = false`);
+				isComposing.current = false;
+			}, 500);
+		};
+
+		msgInput.addEventListener('compositionstart', handleCompositionStart);
+		msgInput.addEventListener('compositionend', handleCompositionEnd);
+
+		return () => {
+			socket.close();
+
+			clearTimeout(timeout_id);
+			msgInput.removeEventListener('compositionstart', handleCompositionStart);
+			msgInput.removeEventListener('compositionend', handleCompositionEnd);
+		}
 	}, []);
 
 	const sendMessage = () => {
@@ -50,10 +79,16 @@ export default function App() {
 			</div>
 			<div className="chat-input">
 				<input
+					id="MsgInput"
+					ref={msgInputRef}
 					value={input}
 					onChange={(e) => setInput(e.target.value)}
-					onKeyUp={(e) => {
-						if (e.key === "Enter") {
+					onKeyDown={(e) => {
+						// console.log({isComposing, key: e.key, keyCode: e.keyCode});
+						if (isComposing.current) return;
+
+						if (e.key == "Enter" && !isComposing.current) {
+							e.preventDefault();
 							sendMessage();
 						}
 					}}
