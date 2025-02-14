7c45bb297a706d93f9018bb3e6598ecbd4bf99f3
jemielttf <4700417+jemielttf@users.noreply.github.com>
2025-02-15 06:55:11 +0900

    updae: Sessionの有効期限を設定

--- backend/RedisChat.php
+++ backend/RedisChat.php
@@ -2,12 +2,15 @@
 
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use React\EventLoop\Loop;
 use React\EventLoop\LoopInterface;
+use Predis\Client as Redis;
 use Clue\React\Redis\RedisClient;
 
 class RedisChat implements MessageComponentInterface
 {
 	protected $clients;
+	protected $redis;
 	protected $redis_publisher;
 	protected $redis_subscriber;
 	protected $loop;
@@ -17,66 +20,105 @@ class RedisChat implements MessageComponentInterface
 	public function __construct(LoopInterface $loop) {
 		$this->clients = new \SplObjectStorage;
 		$this->loop = $loop;
+		$this->redis = new Redis(['host' => 'redis']);
 
 		echo "__construct :: Init RedisChat\n";
 	}
 
 	public function onOpen(ConnectionInterface $conn) {
-		// セッションIDの生成・取得
-		$sessionId = $this->generateSessionId($conn);
-
-		$this->clients->attach($conn);
-		echo "New connection! (Client ID: {$conn->resourceId})\n";
-		echo "New connection! (Session ID: {$sessionId})\n";
-
+		// Redisへの接続を確立
 		if (empty($this->redis_publisher)) {
 			$this->redis_publisher = new RedisClient('redis');
 			$this->redis_publisher->set('greeting', "Hello! Redis");
 
 			// Redis チャンネルを購読
 			$this->subscribeToRedis();
+
+			// 定期的なセッションのクリーンナップ
+			Loop::addPeriodicTimer(60, function () {
+				echo "------------------\n";
+				echo "セッションのクリーンナップ\n";
+				echo "------------------\n";
+
+				$this->redis_publisher->hgetall('active_sessions')->then(
+					function ($sessions) {
+						print_r($sessions);
+						echo "------------------\n";
+
+						for ($i = 0; $i < count($sessions); $i += 2) {
+							$sessionId 		= $sessions[$i];
+							$lastActivity	= $sessions[$i + 1];
+
+							if (time() - $lastActivity > 180) {
+								$this->redis_publisher->hdel('sessions', $sessionId);
+								$this->redis_publisher->hdel('users', $sessionId);
+								$this->redis_publisher->hdel('active_sessions', $sessionId);
+							}
+						}
+					}
+				)->catch(function(\Exception $e) {
+					echo "ERROR!! : {$e->getMessage()}\n";
+				});
+			});
 		}
 
-		$this->redis_publisher->hexists('sessions', $sessionId)->then(function ($exists) use ($conn, $sessionId) {
-			if (!$exists) {
-				// セッション ID が Redis に存在しない場合、新規登録
-				$this->redis_publisher->hset('sessions', $sessionId, json_encode([
-					'resource_id' => $conn->resourceId,
-					'created_at' => time()
-				]))->then(function () use ($conn, $sessionId) {
-					echo "New session created: $sessionId\n";
+		// セッションIDの生成・取得
+		$sessionId = $this->generateSessionId($conn);
 
-					// クライアントに接続情報を送信
-					$conn->send(json_encode([
-						'id'            => $this->msg_id,
-						'type'          => 'session_init',
-						'resource_id'   => $conn->resourceId,
-						'session_id'	=> $sessionId,
-					]));
-					$this->msg_id++;
-				});
-			} else {
-				echo "Existing session found: $sessionId\n";
+		$this->clients->attach($conn);
+		$this->clients->attach($conn, ['session_id' => $sessionId]);
+		echo "New connection! (Client ID: {$conn->resourceId})\n";
+		echo "New connection! (Session ID: {$sessionId})\n";
 
-				// クライアントに接続情報を送信
-				$conn->send(json_encode([
-					'id'            => $this->msg_id,
-					'type'          => 'connection',
-					'resource_id'   => $conn->resourceId,
-					'resource_id'	=> $sessionId,
-				]));
-				$this->msg_id++;
+		// セッションIDの登録の有無に関わらずHSETでクライアントIDの登録 or 更新をする。
+		$this->redis_publisher->hset('sessions', $sessionId, $conn->resourceId)->then(
+			function () use ($conn, $sessionId) {
+				$this->redis_publisher->hexists('users', $sessionId)->then(
+					function ($exists) use ($conn) {
+						$sessionId = $this->clients[$conn]['session_id'];
+
+						if (empty($exists)) {
+							// クライアントに接続情報を送信
+							$conn->send(json_encode([
+								'id'            => $this->msg_id,
+								'type'          => 'session_init',
+								'resource_id'   => $conn->resourceId,
+								'session_id'	=> $sessionId,
+								'error'			=> 0,
+							]));
+						} else {
+							$this->redis_publisher->hget('users', $sessionId)->then(
+								function ($user_name) use ($conn) {
+									$data = [
+										'id'            => $this->msg_id,
+										'type'          => 'user_name',
+										'resource_id'   => $conn->resourceId,
+										'user_name'     => $user_name,
+										'error'			=> 0,
+									];
+
+									$this->publishToRedis($data);
+								}
+							);
+						}
+						$this->msg_id++;
+						$this->updateSessionLastActiveTime($sessionId);
+					})->catch(function(\Exception $e) {
+						echo "ERROR!! : {$e->getMessage()}\n";
+					});
 			}
-		});
+		);
 	}
 
 	public function onMessage(ConnectionInterface $from, $msg) {
 		$msg 	= json_decode($msg);
 		$msg_id = $this->msg_id;
 
+		$sessionId = $this->clients[$from]['session_id'];
+
 		switch ($msg->type) {
 			case 'user_name':
-				$this->redis_publisher->hset('users', $from->resourceId, $msg->user_name)->then(function() use ($from, $msg, $msg_id) {
+				$this->redis_publisher->hset('users', $sessionId, $msg->user_name)->then(function() use ($from, $msg, $msg_id, $sessionId) {
 					$data = [
 						'id'            => $msg_id,
 						'type'          => 'user_name',
@@ -86,6 +128,7 @@ class RedisChat implements MessageComponentInterface
 					];
 
 					$this->publishToRedis($data);
+					$this->updateSessionLastActiveTime($sessionId);
 				}, function(\Exception $e) use ($from, $msg, $msg_id) {
 					$data = [
 						'id'            => $msg_id,
@@ -100,7 +143,12 @@ class RedisChat implements MessageComponentInterface
 				break;
 
 			case 'message':
-				$this->redis_publisher->hget('users', $from->resourceId)->then(function ($user_name) use ($from, $msg, $msg_id) {
+				$this->redis_publisher->hget('users', $sessionId)->then(function ($user_name) use ($from, $msg, $msg_id, $sessionId) {
+					if (empty($user_name)) {
+						$from->close();
+						return;
+					}
+
 					$data = [
 						'id'            => $msg_id,
 						'type'          => 'message',
@@ -111,6 +159,7 @@ class RedisChat implements MessageComponentInterface
 					];
 
 					$this->publishToRedis($data);
+					$this->updateSessionLastActiveTime($sessionId);
 				});
 				break;
 
@@ -137,10 +186,9 @@ class RedisChat implements MessageComponentInterface
 
 	private function generateSessionId(ConnectionInterface $conn) {
 		$queryString = $conn->httpRequest->getUri()->getQuery();
-		echo "queryString : {$queryString}\n";
-		echo "------------------\n";
-
 		parse_str($queryString, $query);
+		echo "query['session_id'] : {$query['session_id']}\n";
+		echo "------------------\n";
 
 		// 既存セッションIDのチェック
 		if (!empty($query['session_id'])) {
@@ -152,11 +200,19 @@ class RedisChat implements MessageComponentInterface
 
 		// 新規セッションID生成
 		$newSessionId = bin2hex(random_bytes(16));
+		$this->redis_publisher->hset('sessions', $newSessionId, $conn->resourceId)->then(function () use ($newSessionId) {
+			echo "New session created: $newSessionId\n";
+		});
+
 		return $newSessionId;
 	}
 
 	private function validateSession($sessionId) {
-		return $this->redis_publisher->hexists('sessions', $sessionId);
+		return $this->redis->hexists('sessions', $sessionId);
+	}
+
+	private function updateSessionLastActiveTime($sessionId) {
+		$this->redis_publisher->hset('active_sessions', $sessionId, time());
 	}
 
 	protected function subscribeToRedis() {
@@ -167,10 +223,12 @@ class RedisChat implements MessageComponentInterface
 
 		$this->redis_subscriber->on('message', function (string $channel, string $payload) {
 			// pubsub message received on given $channel
-			var_dump($channel, json_decode($payload));
-			echo "------------------\n";
 
-			$this->broadcast(json_decode($payload));
+			switch($channel) {
+				case 'chat_channel' :
+					$this->broadcast(json_decode($payload));
+					break;
+			}
 		});
 
 		echo "Subscribing to Redis channel\n";
@@ -187,7 +245,7 @@ class RedisChat implements MessageComponentInterface
 		$this->redis_publisher->ltrim("chat_history", -100, -1);
 
 		echo "------------------\n";
-		$debug_message = $data['message'] ? $data['message'] : '';
+		$debug_message = array_key_exists('message', $data) ? $data['message'] : '';
 		echo "Chat message from ({$data['resource_id']}): {$debug_message}\n";
 		echo "------------------\n";
 	}
